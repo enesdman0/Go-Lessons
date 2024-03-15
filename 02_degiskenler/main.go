821f5c19c27c0b9c32d5f2f0170757bd9d57a02c
Enes Deliduman <eduman931@gmail.com>
2024-03-15 17:43:09 +0300

    Lesson 2

---
+++ 02_degiskenler/main.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	name := "Enes"
+	age := 20
+	isMarried := false
+
+	fmt.Println("Name: ", name, "\n",
+		"Age: ", age, "\n",
+		"Is married: ", isMarried)
+}
