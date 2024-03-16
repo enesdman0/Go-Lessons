59f6869d9d0ed62fcf2a144e46c05e9d7a747846
Enes Deliduman <eduman931@gmail.com>
2024-03-16 16:25:38 +0300

    Lesson 6

---
+++ 06_scope/main.go
@@ -0,0 +1,29 @@
+package main
+
+import "fmt"
+
+// var packVar = "Package Scope"
+
+func main() {
+	// if true {
+	// 	var blockVar = "Block Scope"
+	// 	fmt.Println(blockVar)
+	// }
+	
+	// fmt.Println(blockVar)
+	
+	// var funcVar = "Func Scope"
+	// fmt.Println(funcVar)
+	
+	// fmt.Println(packVar)
+	
+	// myFunc()
+
+	name := "Enes"
+	name, surname := "Aybala","Demir"
+	fmt.Println(name,surname)
+}
+
+// func myFunc() {
+// 	fmt.Println(packVar)
+// }
