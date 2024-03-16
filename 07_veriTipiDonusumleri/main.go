a08fae24af4c95b77b77eef72ceef8ce4754e004
Enes Deliduman <eduman931@gmail.com>
2024-03-16 17:00:50 +0300

    Lesson 7

---
+++ 07_veriTipiDonusumleri/main.go
@@ -0,0 +1,33 @@
+package main
+
+import "fmt"
+
+func main() {
+	// x := 10
+	// y := 10.6
+
+	// fmt.Printf("%v %T\n", x, x)
+	// fmt.Printf("%v %T\n", y, y)
+
+	// Veri tipi dönüşümü,
+
+	// fmt.Println(float64(x) + y)
+	// x := 10
+	// y := 10.6
+	//
+	// fmt.Printf("%v %T\n", x, x)
+	// fmt.Printf("%v %T\n", y, y)
+
+	// var x int8 = 127
+	// var y int16
+
+	// y = int16(x)
+
+	// fmt.Println(y)
+
+	num1 := 106
+	str1 := string(67)
+
+	fmt.Printf("%v %T\n", num1, num1)
+	fmt.Printf("%v %T\n", str1, str1)
+}
