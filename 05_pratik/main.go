cdadde27dce3bea66759ab7d7626828e70ad51fb
Enes Deliduman <eduman931@gmail.com>
2024-03-16 14:37:29 +0300

    Lesson 5

---
+++ 05_pratik/main.go
@@ -0,0 +1,36 @@
+package main
+
+import "fmt"
+
+func main() {
+	// var studentName string = "John Doe"
+	// var grade int = 77
+	// var isPassed bool = true
+
+	// var studentName = "John Doe"
+	// var grade = 77
+	// var isPassed = true
+
+	// studentName := "John Doe"
+	// grade := 77
+	// isPassed := true
+
+	// var studentName, grade, isPassed = "John Doe", 77, true
+	// studentName, grade, isPassed := "John Doe", 77, true
+
+	// var studentName string
+	// studentName = "Enes Duman"
+
+	// Static - synamic data types
+
+	// studentName := 58
+	// fmt.Println(studentName)
+	// fmt.Printf("%T", studentName)
+
+	// studentName = "ENES"
+	// fmt.Printf("%T", studentName)
+
+	// fmt.Println(studentName)
+	// fmt.Println(grade)
+	// fmt.Println(isPassed)
+}
